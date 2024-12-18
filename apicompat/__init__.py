833f5e4406ab05ee99dc86925b0339ef1981442e
nicogodet <nicolas.godet@outlook.fr>
2024-12-18 11:27:50 +0100

    crlf to lf

--- apicompat/__init__.py
+++ apicompat/__init__.py
@@ -6,15 +6,15 @@ import qgis.utils
 # Dealing with a QGIS 2 version, monkey patch some things
 # also guard with initialized so monkeypatching happens only once
 initialized = False
-if not initialized and hasattr(qgis.utils, 'QGis'):
+if not initialized and hasattr(qgis.utils, "QGis"):
     initialized = True
     # log('Running apicompat on QGIS version %s' % QGIS_VERSION)
 
     # Here import all the compatibility fixes modules
-    import qgswkbtypes
-    import qgsproject
-    import qgspointxy
-    import qgsmaplayer
-    import qgsgeometry
     import qgiscoreqgis
     import qgisutilsqgis
+    import qgsgeometry
+    import qgsmaplayer
+    import qgspointxy
+    import qgsproject
+    import qgswkbtypes
--- resources.py
+++ resources.py
@@ -7,7 +7,7 @@
 #
 # WARNING! All changes made in this file will be lost!
 
-#from PyQt4 import QtCore
+# from PyQt4 import QtCore
 from qgis.PyQt import QtCore
 
 qt_resource_data = b"\
@@ -70,10 +70,13 @@ qt_resource_struct = b"\
 \x00\x00\x00\x40\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
 "
 
+
 def qInitResources():
     QtCore.qRegisterResourceData(0x01, qt_resource_struct, qt_resource_name, qt_resource_data)
 
+
 def qCleanupResources():
     QtCore.qUnregisterResourceData(0x01, qt_resource_struct, qt_resource_name, qt_resource_data)
+
 
 qInitResources()
